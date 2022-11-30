ae1a60c2808f620087f28ad62196be9a54af84ef
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-30 16:51:35 -0500

    major styling changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 const Home = (props) => {
-	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
-		<div className="container m-2">
-			<h2>Who is Alina</h2>
+		<div className="center">
+			<h2>Alina Ishizaki</h2>
+			<img class="face" src="my-face.png" alt="Alina's smiling face in a rounded frame. She has shoulder length brown hair"/>
+			<p>I am a Full Stack Engineer looking to help the world with thoughtful and intentional coding design.</p>
+			<h6>
+				Resume<br/>
+				Stuff
+			</h6>
 		</div>
 	)
 }
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -6,57 +6,31 @@ const linkStyle = {
     color: 'white',
     textDecoration: 'none'
 }
-const authenticatedOptions = (
-	<>
-		<Nav.Link>
-			<Link to='change-password' style={linkStyle}>
-				Change Password
-			</Link>
-		</Nav.Link>
-		<Nav.Link>
-			<Link to='sign-out' style={linkStyle}>
-				Sign Out
-			</Link>
-		</Nav.Link>
-	</>
-)
-
-const unauthenticatedOptions = (
-	<>
-        <Nav.Link>
-		    <Link to='sign-up' style={linkStyle}>Sign Up</Link>
-        </Nav.Link>
-        <Nav.Link>
-		    <Link to='sign-in' style={linkStyle}>Sign In</Link>
-        </Nav.Link>
-	</>
-)
 
 const alwaysOptions = (
 	<>
 		<Nav.Link>
 			<Link to='/' style={linkStyle}>
-				Home
+				Projects
 			</Link>
 		</Nav.Link>
 	</>
 )
 
-const Header = ({ user }) => (
+const Header = () => (
 	<Navbar className='container m-2' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                Alina
+                Welcome to my Page of Stuff
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
 		<Navbar.Collapse id='basic-navbar-nav'>
 			<Nav className='ml-auto'>
-				{user && (
-					<span className='navbar-text mr-2'>Welcome, {user.email}</span>
-				)}
+				{/* might use this later */}
+				{/* <span className='navbar-text mr-2'></span> */}
+
 				{alwaysOptions}
-				{user ? authenticatedOptions : unauthenticatedOptions}
 			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
